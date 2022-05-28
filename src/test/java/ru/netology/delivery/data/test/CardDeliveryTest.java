06869e16103be81d75d83cffc078afbff162bb52
Bednova Ekaterina <96745291+BednovaK@users.noreply.github.com>
2022-05-28 12:56:28 +0300

    Allure added

--- src/test/java/ru/netology/delivery/data/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/data/test/CardDeliveryTest.java
@@ -2,6 +2,10 @@ package ru.netology.delivery.data.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import data.DataGenerator;
@@ -14,6 +18,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());}
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");}
 
 
     @Test
